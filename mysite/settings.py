0dd1608f38e244f9257779393b4866f56b177688
maknetaRo <makneta@gmail.com>
2018-08-19 23:19:02 +0200

    created pagination, share posts by email, comments  and tagging

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Sending emails
+EMAIL_BECKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'admin@email.com'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT =587
+EMAIL_USE_TLS = True
